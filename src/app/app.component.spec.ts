a06216292a701e16707a0897b9e7f94714f0e6ba
Coltin Kifer <ck108860@yahoo.com>
2018-02-05 18:39:10 -0500

    mess with tests, add google analytics

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,10 +17,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Coltin Kifer'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Coltin Kifer');
   }));
 
   it('should render title in a h1 tag', async(() => {
